3709f944807bead3794af0c9ce4999c8f2ae5286
haxi0 <fabiomailboxgood@mail.ru>
2023-11-28 00:15:34 +0300

    blahblahblah

---
+++ TipsGotTrolled/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TipsGotTrolled
+//
+//  Created by haxi0 on 28.11.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TipsGotTrolled/TipsGotTrolledApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipsGotTrolledApp.swift
+//  TipsGotTrolled
+//
+//  Created by haxi0 on 28.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct TipsGotTrolledApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
