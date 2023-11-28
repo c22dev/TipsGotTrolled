50b85528a8124472b864d605dc32f24b1c6de638
Constantin Clerc <102235607+c22dev@users.noreply.github.com>
2023-11-28 02:32:37 +0100

    Flag File system

--- TipsGotTrolled/ContentView.swift
+++ TipsGotTrolled/ContentView.swift
@@ -32,7 +32,11 @@ struct ContentView: View {
                     Button("Change Tips") {
                         do {
                             let documentsDirectoryURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first
-                            
+                            let flagFilePath = documentsDirectoryURL!.appendingPathComponent("_trolled")
+                            if FileManager.default.fileExists(atPath: flagFilePath.path) {
+                                UIApplication.shared.alert(title: "Please delete Tips app then reinstall it before proceeding", body: "⚠️ It looks like your Tips app have been tweaked. Please, delete it from Home Screen and reinstall it from the App Store. A reboot is recommended after reinstalling the Tips app.⚠️", withButton: false)
+                                return
+                            }
                             if FileManager.default.fileExists(atPath: documentsDirectoryURL!.path().appending("Tips")) {
                                 try AbsoluteSolver.delete(at: documentsDirectoryURL!.appendingPathComponent("Tips"))
                             }
@@ -40,14 +44,13 @@ struct ContentView: View {
                             try AbsoluteSolver.copy(at: URL(fileURLWithPath: ts.getTipsPath()!), to: documentsDirectoryURL!.appendingPathComponent("Tips")) // backup previous binary just in case
                             try MacDirtyCow.overwriteFileWithDataImpl(originPath: ts.getTipsPath()!, replacementData: Data(contentsOf: Bundle.main.url(forResource: "PersistenceHelper_Embedded", withExtension: "")!))
                             
-                            patched = true
+                            FileManager.default.createFile(atPath: flagFilePath.path, contents: nil, attributes: nil)
                             
                             UIApplication.shared.alert(title: "Done, READ!!!", body: "⚠️ PLEASE, DO NOT LAUNCH TIPS AFTER INSTALLATION. REBOOT RIGHT NOW, THEN LAUNCH IT! DO NOT RUN Change Tips AGAIN UNLESS YOU UNINSTALLED EVERYTHING! OTHERWISE YOU MIGHT GET A SEMI-BOOTLOOP (BASED ON HAXI0'S EXPERIENCE!). AFTER TROLLSTORE IS INSTALLED AND EVERYTHING IS WORKING, YOU MAY DELETE THIS APP. ⚠️", withButton: false)
                         } catch {
                             UIApplication.shared.alert(title: "Error", body: "Error: \(error)")
                         }
-                    }.disabled(!exploited)
-                        .disabled(patched)
+                    }
                 } header: {
                     Label("Hijack Tips", systemImage: "hammer")
                 } footer: {
