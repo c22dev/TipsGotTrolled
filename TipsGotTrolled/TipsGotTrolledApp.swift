93c3233bee73a00f81ed3a2700ed5ca693f6fb58
Constantin Clerc <102235607+c22dev@users.noreply.github.com>
2023-11-28 02:36:30 +0100

    First Time Warning

--- TipsGotTrolled/TipsGotTrolledApp.swift
+++ TipsGotTrolled/TipsGotTrolledApp.swift
@@ -9,8 +9,13 @@ import SwiftUI
 
 @main
 struct TipsGotTrolledApp: App {
+    @AppStorage("isFirstLaunch") var isFirstLaunch: Bool = true
     var body: some Scene {
         WindowGroup {
+            if isFirstLaunch {
+                UIApplication.shared.alert(title: "Please delete Tips app then (re)install it before proceeding", body: "Hi ! It looks like it's the first time you come here. To avoid any corruption, please delete Tips app from Home Screen then reinstall it. Thanks for using this tool!", withButton: true)
+                isFirstLaunch = false
+            }
             ContentView()
         }
     }
