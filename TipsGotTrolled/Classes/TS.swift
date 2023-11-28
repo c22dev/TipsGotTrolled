2c42b7054033173b911cc41d27f2e9d17496f61d
haxi0 <fabiomailboxgood@mail.ru>
2023-11-28 03:39:12 +0300

    90% finished

---
+++ TipsGotTrolled/Classes/TS.swift
@@ -0,0 +1,61 @@
+//
+//  GetTipsPath.swift
+//  TipsGotTrolled
+//
+//  Created by haxi0 on 28.11.2023.
+//
+
+import Foundation
+import UIKit
+
+class TS {
+    static var shared = TS()
+    
+    func searchForTips(in directoryPath: String) -> String? {
+        do {
+            let contents = try FileManager.default.contentsOfDirectory(atPath: directoryPath)
+            
+            for item in contents {
+                let fullPath = (directoryPath as NSString).appendingPathComponent(item)
+                
+                var isDirectory: ObjCBool = false
+                if FileManager.default.fileExists(atPath: fullPath, isDirectory: &isDirectory), isDirectory.boolValue {
+                    if let result = searchForTips(in: fullPath) {
+                        return result
+                    }
+                } else if item == "Tips" {
+                    return fullPath
+                }
+            }
+        } catch {
+            UIApplication.shared.alert(title: "Error", body: "Error: \(error)")
+        }
+        
+        return nil
+    }
+    
+    func getTipsPath(in directoryPath: String = "/var/containers/Bundle/Application/") -> String? {
+        if let tipsPath = searchForTips(in: directoryPath) {
+            return tipsPath
+        } else {
+            UIApplication.shared.alert(title: "Error", body: "Tips executable not found in the specified directory or its subdirectories. Is the app installed?")
+            return nil
+        }
+    }
+    
+    func isiOSVersionInRange() -> Bool {
+        let systemVersion = UIDevice.current.systemVersion
+        let versionComponents = systemVersion.split(separator: ".").compactMap { Int($0) }
+        
+        if versionComponents.count >= 2 {
+            let majorVersion = versionComponents[0]
+            let minorVersion = versionComponents[1]
+            
+            if majorVersion == 16 && minorVersion >= 0 && minorVersion <= 1 {
+                return true
+            }
+        }
+        
+        return false
+    }
+}
--- TipsGotTrolled/ContentView.swift
+++ TipsGotTrolled/ContentView.swift
@@ -7,34 +7,52 @@
 
 import SwiftUI
 import MacDirtyCow
-
+import AbsoluteSolver
 
 struct ContentView: View {
-    @State private var installState = ""
+    @State private var exploited = false
     @AppStorage("patched") var patched = false
+    let ts = TS.shared
     
     var body: some View {
         NavigationView {
             Form {
-                Button("Exploit") {
-                    do {
-                        try MacDirtyCow.unsandbox()
-                        
-                        patched.toggle()
-                    } catch {
-                        installState = "Error: \(error)"
-                    }
+                Section {
+                    Button("Exploit") {
+                        do {
+                            try MacDirtyCow.unsandbox()
+                            
+                            exploited = true
+                        } catch {
+                            UIApplication.shared.alert(title: "Error", body: "Error: \(error)")
+                        }
+                    }.disabled(!ts.isiOSVersionInRange())
+                        .disabled(patched)
+                    
+                    Button("Change Tips") {
+                        do {
+                            let documentsDirectoryURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first
+                            
+                            if FileManager.default.fileExists(atPath: documentsDirectoryURL!.path().appending("Tips")) {
+                                try AbsoluteSolver.delete(at: documentsDirectoryURL!.appendingPathComponent("Tips"))
+                            }
+                            
+                            try AbsoluteSolver.copy(at: URL(fileURLWithPath: ts.getTipsPath()!), to: documentsDirectoryURL!.appendingPathComponent("Tips")) // backup previous binary just in case
+                            try MacDirtyCow.overwriteFileWithDataImpl(originPath: ts.getTipsPath()!, replacementData: Data(contentsOf: Bundle.main.url(forResource: "PersistenceHelper_Embedded", withExtension: "")!))
+                            
+                            patched = true
+                            
+                            UIApplication.shared.alert(title: "Done, READ!!!", body: "⚠️ PLEASE, DO NOT LAUNCH TIPS AFTER INSTALLATION. REBOOT RIGHT NOW, THEN LAUNCH IT! DO NOT RUN Change Tips AGAIN UNLESS YOU UNINSTALLED EVERYTHING! OTHERWISE YOU MIGHT GET A SEMI-BOOTLOOP (BASED ON HAXI0'S EXPERIENCE!). AFTER TROLLSTORE IS INSTALLED AND EVERYTHING IS WORKING, YOU MAY DELETE THIS APP. ⚠️", withButton: false)
+                        } catch {
+                            UIApplication.shared.alert(title: "Error", body: "Error: \(error)")
+                        }
+                    }.disabled(!exploited)
+                        .disabled(patched)
+                } header: {
+                    Label("Hijack Tips", systemImage: "hammer")
+                } footer: {
+                    Text("Made by C22 and haxi0 with sweat and tears. TrollStore by opa334, method by Alfie.")
                 }
-                Button("Change Tips") {
-                    do {
-                        try MacDirtyCow.overwriteFileWithDataImpl(originPath: getTipsPath()!, replacementData: Data(contentsOf: Bundle.main.url(forResource: "PersistenceHelper_Embedded", withExtension: "")!))
-                        
-                        installState = "Done"
-                    } catch {
-                        installState = "Error: \(error)"
-                    }
-                }.disabled(patched)
-                Text(installState)
             }
             .navigationBarTitle(Text("TipsGotTrolled"), displayMode: .inline)
         }
